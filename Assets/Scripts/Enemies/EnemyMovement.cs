60cf2ccab24241b9e2eaab96be9301e4779c0913
Henrique Ribeiro <gamingwithapotato@gmail.com>
2022-11-30 14:41:57 +0000

    Remove return placed for debugging

--- Assets/Scripts/Enemies/EnemyMovement.cs
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -49,7 +49,6 @@ namespace Enemies
     
         private void NextPosition()
         {
-            return;
             if (wasRunning) animator.SetTrigger(Idle);
             if (timeStopped > 1.0f && timeStopped < 3.0f && navMeshAgent.velocity == new Vector3(0, 0, 0) && InWalkingState())
             {
