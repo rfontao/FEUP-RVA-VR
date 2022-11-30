cd4a47d7abd66108725a75ad3618771de8a95730
rfontao <ricardo.fontao@gmail.com>
2022-11-30 16:15:39 +0000

    Added lantern that produces light

--- Assets/Scripts/SetSkybox.cs
+++ Assets/Scripts/SetSkybox.cs
@@ -4,25 +4,24 @@ using UnityEngine;
 [RequireComponent(typeof(Skybox))]
 public class SetSkybox : MonoBehaviour
 {
-
     [SerializeField] private Material daySkybox;
     [SerializeField] private Material nightSkybox;
-    
+
     private void Start()
     {
         var now = DateTime.Now;
         var skybox = GetComponent<Skybox>();
-        
-        if(now.Hour is > 18 or < 6){
+
+        if (now.Hour is > 18 or < 6)
+        {
             skybox.material = nightSkybox;
             var light = GameObject.Find("Directional Light");
             light.SetActive(false);
             RenderSettings.ambientIntensity = 0.15f;
         }
-        else{
+        else
+        {
             skybox.material = daySkybox;
         }
-        
     }
-    
 }
