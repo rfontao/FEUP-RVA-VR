6cc5eac28415f8f946b87851656f3078bcb1eba4
rfontao <ricardo.fontao@gmail.com>
2022-12-10 23:37:00 +0000

    Added player respawning

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -1,12 +1,11 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
     [SerializeField] private List<GameObject> hearts;
+
+    [SerializeField] private Transform respawnPoint;
 
     private int numberHits;
     private float immunityTime;
@@ -14,13 +13,12 @@ public class PlayerCollision : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(immunityTime < 20f) //prevents overflow in case the user is not hit in a very long time
+        if (immunityTime < 20f) //prevents overflow in case the user is not hit in a very long time
             immunityTime += Time.deltaTime;
     }
 
@@ -32,5 +30,21 @@ public class PlayerCollision : MonoBehaviour
             hearts[numberHits].SetActive(false);
             numberHits++;
         }
+
+        if (numberHits == 3)
+        {
+            RespawnPlayer();
+        }
+    }
+
+    private void RespawnPlayer()
+    {
+        foreach (var heart in hearts)
+        {
+            heart.SetActive(true);
+        }
+
+        numberHits = 0;
+        transform.parent.parent.position = respawnPoint.position;
     }
 }
